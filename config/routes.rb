b453f66d0f2c5718d97d1e466bf2b58d07d6f728
ocabello <ocabello2@gmail.com>
2015-10-22 03:29:33 +0000

    tried to fix createPost routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/signup'
   get 'pages/feed'
-  get '/createPost' => "pages#createPost"
+  get 'pages/createPost' => "pages#createPost"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
