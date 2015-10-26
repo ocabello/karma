a55c1e0b212a5015a5929ff244fb9334f2927d82
ocabello <ocabello2@gmail.com>
2015-10-26 00:30:00 +0000

    Added user table for db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   def home
   end
+  
   def signup
+    @user=UserInfo.new(user_params)
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:firstName)
   end
   def feed
   end
