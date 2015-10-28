65ee895ea282ba5edc2a4eb5d6d418f6136b9348
Kai Kuroda <kkuroda27@gmail.com>
2015-10-28 06:14:53 +0000

    added posts controller/model, added index,create,new functionality and view pages as well. Basically, posts are stored now. I just redid it without scaffolding so it is a lot cleaner now

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+    
+    def index 
+        @posts = Post.all 
+    end
+    
+    def new 
+        @post = Post.new
+    end
+    
+    def create 
+      @post = Post.new(post_params) 
+      if @post.save 
+        redirect_to '/posts' 
+      else 
+        render 'new' 
+      end 
+    end
+
+    private
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def post_params
+            params.require(:post).permit(:title, :description, :postedBy)
+        end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028032817) do
+ActiveRecord::Schema.define(version: 20151028044152) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "postedBy"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
