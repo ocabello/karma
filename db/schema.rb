21d55fece8e3873321856d06bd06df4db6e5d735
Kai Kuroda <kkuroda27@gmail.com>
2015-10-28 03:58:56 +0000

    Oops. Forgot to add untracked files. Now I added them

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026011111) do
+ActiveRecord::Schema.define(version: 20151028032817) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -22,5 +22,12 @@ ActiveRecord::Schema.define(version: 20151026011111) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
