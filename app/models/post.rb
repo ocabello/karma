9fad14cc28cfdd1513921cc2025a32d02dc81208
Gianna Capezio <gcapezio@tulane.edu>
2015-11-19 00:20:19 +0000

    post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
     attr_accessor :current_user_name
     attr_accessor :poster_name
     belongs_to :user
+    validates_presence_of :title
+    validates_presence_of :description
+    
 end
