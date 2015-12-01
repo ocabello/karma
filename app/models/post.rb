3ee5262d7010442c772b0bb5896c99978de2e4c5
Gianna Capezio <gcapezio@tulane.edu>
2015-12-01 00:47:27 +0000

    New form for adding and editing post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
 
     belongs_to :user
-    validates :title, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters" }
-    validates :description, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters" }
+    validates :title, format: { with: /\A[a-zA-Z0-9 .!?"-]+\z/,
+    message: "Error: regular expressions only" }
+    validates :description, format: { with: /\A[a-zA-Z0-9 .!?"-]+\z/,
+    message: "Error: regular expressions only" }
     
 end
